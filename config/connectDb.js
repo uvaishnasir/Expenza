5b3ad6016fc1c519dd4bc8cd43184a9d59fcca0f
Uvaish <uvaish.nasir@handajitech.com>
2024-10-08 00:52:07 +0530

    set strictQuery

--- config/connectDb.js
+++ config/connectDb.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
+
 const connectDb = async () => {
   try {
+    // Set the strictQuery option
+    mongoose.set('strictQuery', true); // or false, based on your preference
+
     const connectionInstance = await mongoose.connect(process.env.MONGO_URL);
     console.log(
       `\nMongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const morgan = require("morgan");
+// const morgan = require("morgan");
 const dotenv = require("dotenv");
 const path = require("path");
 const connectDb = require("./config/connectDb");
@@ -22,22 +22,17 @@ connectDb()
   });
 
 //middlewares
-app.use(morgan("dev"));
+// app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors());
 
 //routes declaration.
-//user routes
 app.use("/api/v1/users", require("./routes/userRoute"));
-//transections routes
 app.use("/api/v1/transections", require("./routes/transectionRoutes"));
 
 //static files
 app.use(express.static(path.join(__dirname, "./client/build")));
 
-app.get("/hello", (req,res)=>{
-  res.send("Hello World!");
-})
 
 // app.get("*", function (req, res) {
 //   res.sendFile(path.join(__dirname, "./client/build/index.html"));
